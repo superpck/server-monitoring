307a14d1a1935a87d32e0fa346233c726541e0cc
superpck <superpck@gmail.com>
2026-03-22 20:20:11 +0700

    change token name

--- frontend/src/app/configs/config.ts
+++ frontend/src/app/configs/config.ts
@@ -1,9 +1,9 @@
 import { version } from '../../../package.json';
 
 export default {
-  appName: 'Server Management',
+  appName: 'Server Monitoring',
   version,
   subVersion: '2026.3.22-1',
-  tokenName: 'server-management-token',
+  tokenName: 'server-monitoring-token',
   apiUrl: 'http://localhost:4000',
 }
